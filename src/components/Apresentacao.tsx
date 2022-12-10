83800feaa15cc3bf750c51db0e24a9d6b6322e56
mazzillio <mateca500@gmail.com>
2022-12-10 10:58:29 -0300

    Mudanças na header

--- src/components/Apresentacao.tsx
+++ src/components/Apresentacao.tsx
@@ -2,7 +2,7 @@ import { Button } from "./Button";
 
 export function Apresentacao() {
   return(
-    <div className="w-full pl-20 pr-40 mt-20 flex flex-col items-center">
+    <div className="w-full mt-20 flex flex-col items-center">
       <article className=" text-white text-7xl mb-6 leading-normal flex items-center flex-col">
         <p className="text-left w-full">Forje <span className="text-primary-300">conexões</span> com seu <span className="text-primary-300">público</span></p>
         <p className="text-left w-full">através de um desgin <span className="text-primary-300">estratégico</span></p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,17 @@ export function Header() {
     window.location.href=`/#${param}`;
   };    
   return(
-    <header className="flex flex-row justify-between bg-transparent w-full ml-2 pt-5 px-7">
-      <Image 
-        src={logo} 
-        alt="icone do igneo studios" 
-        height={125}
-        className="mt-4 mb-2"
-      />     
-      <nav className="w-1/3 mt-5 text-white">
-        <ul className="w-full list-none flex flex-row justify-evenly">
+    <header className="flex flex-row justify-between bg-transparent ml-2 pt-5">
+      <div className="pl-36 mt-4 mb-2">
+        <Image 
+          src={logo} 
+          alt="icone do igneo studios" 
+          height={125}
+          width={175}
+        />
+      </div>     
+      <nav className="mt-5 text-white w-1/3 px-10 text-4xl">
+        <ul className="w-full list-none flex flex-row justify-between">
           <li 
             className="hover:cursor-pointer hover:text-primary-300" 
             onClick={()=> click("sobre")}
